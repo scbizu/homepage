bfc5811d3e8ba8a958a88938d29525ae13782b72
scnace <scbizu@gmail.com>
2021-01-31 20:27:09 +0800

    all: support wasm buildings

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+// Copyleft © 2021 scnace(scbizu@gmail.com)
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
